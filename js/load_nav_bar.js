b249231fb3168c1b2f110bab6e23b23e74fa6bee
Chris <chrismoloci@gmail.com>
2025-10-10 12:16:04 -0400

    Started work on bio.html, created bio.css, modified component loaders to load based on a HTML element rather than specifically to a file

--- js/load_nav_bar.js
+++ js/load_nav_bar.js
@@ -3,6 +3,6 @@
 import { loadTemplate } from './html_loader.js'; // Import the template loader
 
 const navBar = document.getElementById('nav-bar'); // Get the nav-bar element
-const navBarTemplate = await loadTemplate('nav-bar-template', 'components/nav_bar.html'); // Load the nav-bar template
+const navBarTemplate = await loadTemplate('nav-bar-template', '/components/nav_bar.html'); // Load the nav-bar template
 
 navBar.innerHTML = navBarTemplate.innerHTML; // Insert the nav-bar template into the nav-bar element
