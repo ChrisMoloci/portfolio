8ac7fab875518f28e3d2217034946a0256158998
Chris <chrismoloci@gmail.com>
2025-12-04 15:30:44 -0500

    Added JavascriptDoc comments to template.js

--- projects/projectTemplate/template.js
+++ projects/projectTemplate/template.js
@@ -13,23 +13,31 @@ let currentNav;
 
 content.innerHTML = content1.innerHTML; // Init
 
+/**
+ * @description Switches the main content based on the content nav bar clicks
+ * @author Christian Moloci
+ */
 navBar.addEventListener('click', (event) => {
     if (event.target.tagName !== "DIV") return; // Only respond to div clicks
     if (event.target.id === currentNav) return; // Do nothing if the same nav is clicked
 
     currentNav = event.target.id; // Update current nav so we can ignore same click
 
+    // Remove the "selected" class from all the nav buttons
     Array.from(navBar.children).forEach(child => {
         child.classList.remove('selected');
     })
 
     if (event.target.id === "nav-1") {
+        // Show content 1
         content.innerHTML = content1.innerHTML;
-        event.target.classList.add('selected');
+        event.target.classList.add('selected'); // Set nav button to "selected" class
     } else if (event.target.id === "nav-2") {
+        // Show content 2
         content.innerHTML = content2.innerHTML;
-        event.target.classList.add('selected');
+        event.target.classList.add('selected'); // Set nav button to "selected" class
     } else {
+        // For unknown nav items, log an error
         console.error("Unknown nav item clicked");
     }
 });
