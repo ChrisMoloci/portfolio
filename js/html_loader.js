582130dc4fd301a21286920e941f04d3612f6c87
Chris <chrismoloci@gmail.com>
2025-12-03 20:30:08 -0500

    Added documentation and renamed some vars in html_loader to make it easier to read

--- js/html_loader.js
+++ js/html_loader.js
@@ -1,22 +1,31 @@
+/* 
+    Author: Christian Moloci
+    Helper function to parse HTML data from other HTML files
+*/
+
+// Template ID is the id of the tag to get and file is the path to the HTML file
 export async function loadTemplate(templateId, file) {
+    // Since we are working with files, we should use a try catch
     try {
-        const response = await fetch(file);
-        const html = await response.text();
-        const temp = document.createElement('div');
-        temp.innerHTML = html;
-        const documentFragment = temp.querySelector(`#${templateId}`).content.children;
+        // https://developer.mozilla.org/en-US/docs/Web/API/Response/text
 
-        // console.log(documentFragment);
+        const response = await fetch(file); // Fetch the file
+        const html = await response.text(); // Get the text from the file
+        const temp = document.createElement('div'); // Create a temporary div to hold the HTML
+        temp.innerHTML = html; // Set the innerHTML of the temp div to the fetched HTML
 
-        const template = document.createElement('div');
+        // Finds the specific element in temp based on the templateId
+        // This needs to be done this way since querySelector.content returns a document fragment which is a fancy array of DOM objects
+        const templateContentArray = temp.querySelector(`#${templateId}`).content.children;
 
-        for (let fragment of documentFragment) {
-            template.appendChild(fragment.cloneNode(true));
+        const template = document.createElement('div'); // Will store the actual template
+
+        // Append each element form templateContentArray to template
+        for (let el of templateContentArray) {
+            template.appendChild(el.cloneNode(true));
         }
 
-        // console.log(template);
-
-        return template;
+        return template; // Return the template
     } catch (error) {
         console.error("Error loading template:", error);
         return null;
