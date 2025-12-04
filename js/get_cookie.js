63c980b9ebcc3d8213796cab567cb9c9352584f8
Chris <chrismoloci@gmail.com>
2025-12-03 20:07:34 -0500

    Added documentaiton to get_cookie.js

--- js/get_cookie.js
+++ js/get_cookie.js
@@ -1,12 +1,22 @@
-export function getCookieAsArray(name) {
-    const cookies = document.cookie.split('; ');
-    let cookie;
+/* 
+    Author: Christian Moloci
+    Helper function to get a specific cookie based on its name
+*/
 
+// Returns a cookie
+export function getCookieAsArray(name) {
+    // Get all the cookies and split them into an array
+    const cookies = document.cookie.split('; ');
+    let cookie; // Will hold the cookie
+
+    // Iterate through our cookies array
     cookies.forEach(c => {
+        // Split the cookie one more time to get the key and value
         if (c.split('=')[0] === name) {
-            cookie = c.split('=');
-            return;
+            // If the key matches name, this is our cookie
+            cookie = c.split('='); // turn cookie into an array with a key and value
+            return; // Stop iterating
         }
     });
-    return cookie;
+    return cookie; // Return the cookie array or null
 }
