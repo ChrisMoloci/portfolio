6b94384811f4172cabdb385a9d27165c97d7d213
Chris <chrismoloci@gmail.com>
2025-12-04 15:16:01 -0500

    Updated JavaScriptDoc comments on theme_engine.js

--- js/theme_engine.js
+++ js/theme_engine.js
@@ -8,39 +8,49 @@ import { getCookieAsArray } from "/js/get_cookie.js"
 // Themeing based on: https://webdesign.tutsplus.com/color-schemes-with-css-variables-and-javascript--cms-36989t
 
 let cookie; // Will hold the theme cookie
+
 const themeTable = [
     "auto", "light", "dark"
 ] // Tracks class names for :root to set document elemet to
+
 const iconTable = [
     "routine", "light_mode", "dark_mode"
-]; // Tracks icon names for the set color theme
-let themeIndex = 0;
+]; // Tracks icon names for the set color theme (icons from google icons)
+
+let themeIndex = 0; // Holds the current theme index to grab the theme from themeTable
 
 // Get nav bar since it exists from init, we'll grab events from here and filter theme button events
 const navBar = document.getElementById('nav-bar'); 
 
 // Try to get the cookie and set the default theme
 try {
-    cookie = getCookieAsArray('theme');
-    themeIndex = parseInt(cookie[1]);
-    setTheme(themeIndex); // Set the last theme
+    cookie = getCookieAsArray('theme'); // Get the cookie called theme
+    themeIndex = parseInt(cookie[1]); // Convert the theme value to an int and set themeIndex to it
+    setTheme(themeIndex); // Set the initial theme to the last stored one
 } catch (e) {
+    // Commonly runs when cookie does not exist (first time on the site)
     console.error('Could not retrieve cookies, setting theme to default theme:', e);
-    cookie = getCookieAsArray('theme');
     setTheme(0); // Set to default theme
 } finally {
     // Checks for system theme changes and updates if theme is set to auto
     window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', setAutoTheme);
 
-    // Waits for the nav bar DOM to be loaded before attempting to set the icon
+    /**
+     * @description Waits for the nav bar DOM to be loaded before attempting to set the icon in its child
+     * @author Christian Moloci
+     */
     new MutationObserver(() => {
         // More on mutation observer: https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
-        console.log(navBar.cloneNode);
         navBar.querySelector('#theme-icon').textContent = iconTable[themeIndex];
     }).observe(navBar, { childList: true, subtree: false });
 }
 
-// Sets the theme based on the theme index
+/**
+ * @description Sets the theme based on the theme index
+ * @param {*} themeIndex 
+ * @returns 
+ * @author Christian Moloci
+ */
 function setTheme(themeIndex) {
     // Validate theme index value before setting
     if (!(themeIndex >= 0 && themeIndex < themeTable.length)) {
@@ -61,7 +71,10 @@ function setTheme(themeIndex) {
     }
 }
 
-// Sets the theme based on system preference if theme is set to auto
+/**
+ * @description Sets the theme based on system preference if theme is set to auto
+ * @author Christian Moloci
+ */
 function setAutoTheme() {
     // Check if window.matchMedia exists on the browser
     if (window.matchMedia) {
@@ -77,7 +90,10 @@ function setAutoTheme() {
     }
 }
 
-// Listens for theme toggle clicks
+/**
+ * @description Listens for theme toggle clicks and updates theme
+ * @author Christian Moloci
+ */
 navBar.addEventListener('click', (event) => {
     // Ensures el is always #theme-toggle even if event was on it's child
     const el = event.target.closest("#theme-toggle");
