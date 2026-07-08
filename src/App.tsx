52f4ae48f77d466ed5e6996fc2cb9f6122006588
Chris <chrismoloci@gmail.com>
2026-07-08 13:25:12 -0400

    Created Header and Footer and added them to App root.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
 import {Outlet} from "react-router";
+import Header from "./components/Header/Header.tsx";
+import Footer from "./components/Footer/Footer.tsx";
 
 function App() {
     return (
         <>
+            <Header />
             <Outlet/>
+            <Footer/>
         </>
     )
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+// import styles from "./Header.module.css"
+
+function Header() {
+    return (
+        <>
+            <h1>Header</h1>
+        </>
+    )
+}
+
+export default Header
