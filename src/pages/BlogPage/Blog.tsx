885a0f5cdc8ad7553db139638a501b5d2515e593
Chris <chrismoloci@gmail.com>
2026-07-14 19:01:42 -0400

    Began working on ProjectsPage and BlogPage

--- src/pages/BlogPage/Blog.tsx
+++ src/pages/BlogPage/Blog.tsx
@@ -1,9 +1,48 @@
-// import styles from "./Blog.module.css"
+import styles from "./Blog.module.css"
+import LinkButton from "../../components/LinkButton/LinkButton.tsx";
+import BlogCard from "../../components/BlogCard/BlogCard.tsx";
 
 function About() {
     return (
         <>
-            <h1>Blog</h1>
+            <main className={styles.main}>
+                <div className={styles.header}>
+                    <div className={styles.title}>
+                        <h1>Blog</h1>
+
+                        <LinkButton link={"#"} text={"GitHub"} />
+                    </div>
+                </div>
+
+                <div className={styles.content}>
+                    <BlogCard
+                        url={"#"}
+                        title={"Blog Card Card"}
+                        author={"First Last"}
+                        date={"July 14, 2026"}
+                        description={"Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."}
+                        thumbnail={{url: "placeholder.png", alt: ""}}
+                    />
+
+                    <BlogCard
+                        url={"#"}
+                        title={"Blog Card Card"}
+                        author={"First Last"}
+                        date={"July 14, 2026"}
+                        description={"Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."}
+                        thumbnail={{url: "placeholder.png", alt: ""}}
+                    />
+
+                    <BlogCard
+                        url={"#"}
+                        title={"Blog Card Card"}
+                        author={"First Last"}
+                        date={"July 14, 2026"}
+                        description={"Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."}
+                        thumbnail={{url: "placeholder.png", alt: ""}}
+                    />
+                </div>
+            </main>
         </>
     )
 }
--- src/pages/ProjectsPage/Projects.tsx
+++ src/pages/ProjectsPage/Projects.tsx
@@ -1,9 +1,57 @@
-// import styles from "./Projects.module.css"
+import styles from "./Projects.module.css"
+import ProjectCard from "../../components/ProjectCard/ProjectCard.tsx";
+import LinkButton from "../../components/LinkButton/LinkButton.tsx";
 
 function Projects() {
     return (
         <>
-            <h1>Projects</h1>
+            <main className={styles.main}>
+                <div className={styles.header}>
+                    <div className={styles.title}>
+                        <h1>Projects</h1>
+
+                        <LinkButton link={"#"} text={"GitHub"} />
+                    </div>
+                </div>
+
+                <div className={styles.content}>
+                    <ProjectCard
+                        url={"#"}
+                        thumbnail={{
+                            url: "placeholder.png",
+                            alt: ""
+                        }}
+                        title={"Project Card"}
+                        date={"2026"}
+                        description={"Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."}
+                        languages={["html", "css", "js", "react"]}
+                    />
+
+                    <ProjectCard
+                        url={"#"}
+                        thumbnail={{
+                            url: "placeholder.png",
+                            alt: ""
+                        }}
+                        title={"Project Card"}
+                        date={"2026"}
+                        description={"Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."}
+                        languages={["html", "css", "js", "react"]}
+                    />
+
+                    <ProjectCard
+                        url={"#"}
+                        thumbnail={{
+                            url: "placeholder.png",
+                            alt: ""
+                        }}
+                        title={"Project Card"}
+                        date={"2026"}
+                        description={"Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."}
+                        languages={["html", "css", "js", "react"]}
+                    />
+                </div>
+            </main>
         </>
     )
 }
