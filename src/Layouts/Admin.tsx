997f801c777a42eec60ce55e17f80a722dbbd6ba
Chris <chrismoloci@gmail.com>
2026-07-21 15:09:19 -0400

    Removed Header from Admin layout.

--- src/Layouts/Admin.tsx
+++ src/Layouts/Admin.tsx
@@ -1,13 +1,10 @@
 import styles from "./Admin.module.css"
 import {Outlet} from "react-router";
-import Header from "../components/Header/Header.tsx";
 import AdminSidebar from "../components/AdminSidebar/AdminSidebar.tsx";
 
 function Admin() {
     return (
         <>
-            <Header />
-
             <div className={styles.adminLayout}>
                 <AdminSidebar />
                 <Outlet/>
