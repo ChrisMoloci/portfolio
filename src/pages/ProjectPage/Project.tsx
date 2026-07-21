deb55cc72e3c9fc1c69524356e0440704bb5253d
Chris <chrismoloci@gmail.com>
2026-07-21 01:37:05 -0400

    Implemented API states for loading, failed, and success in ProjectPage.

--- src/pages/ProjectPage/Project.tsx
+++ src/pages/ProjectPage/Project.tsx
@@ -5,65 +5,85 @@ import {useEffect, useState} from "react";
 import type {Project} from "../../types/Project.ts";
 import transformProject from "../../transformers/transformProject.ts";
 import {api} from "../../api/client.ts";
+import type {ApiState} from "../../types/ApiState.ts";
 
 function Project() {
     const { slug } = useParams();
-    const [ projectData, setProjectData ] = useState<Project>();
-    const featuredImageURL = import.meta.env.VITE_MEDIA_DIR + "/" + projectData?.featuredImage?.storageKey + ".webp"
+    const [ projectData, setProjectData ] = useState<ApiState<Project>>({ status: "loading" });
+
+    let featuredImageURL = "/src/assets/images/placeholder.png";
+
+    if (projectData.status === "success" && projectData.data.featuredImage) {
+        featuredImageURL = import.meta.env.VITE_MEDIA_DIR + "/" + projectData.data.featuredImage?.storageKey + ".webp"
+    }
 
     const fetchProjectData = async () => {
-        const response = await api.get(`/projects/${slug}`);
+        try {
+            const response = await api.get(`/projects/${slug}`);
 
-        const data = transformProject(response.data);
+            const data: ApiState<Project> =  {
+                status: "success",
+                data: transformProject(response.data)
+            };
 
-        setProjectData(data);
+            setProjectData(data);
+        } catch (error: any) {
+            setProjectData({ status: "error", error: error.messsage });
+        }
     }
 
     useEffect(() => {
         fetchProjectData();
-    })
+    }, []);
 
     return (
         <main className={styles.main}>
-            {/* Header */}
-            <div className={styles.header}>
-                <h2 className={styles.subHeading}>{projectData?.category.name}</h2>
-                <h1 className={styles.heading}>{projectData?.name}</h1>
-                <span className={styles.authorDate}>
-                    <span className={styles.author}>{projectData?.author.name}</span>
-                    &bull;
-                    <span className={styles.date}>{projectData?.createdAt.toDateString()}</span>
-                </span>
-            </div>
-
-            {/* Main Content */}
-            <div className={styles.project}>
-                {/* Article */}
-                <div className={styles.content}>
-                    <img src={featuredImageURL} alt={projectData?.featuredImage?.alt}/>
-
-                    <div className={styles.markdown}>
-                        {/* TODO: Implement MDX Parsing */}
-                        <Markdown>{projectData?.content}</Markdown>
+            {projectData.status === "success" &&
+                <>
+                    {/* Header */}
+                    <div className={styles.header}>
+                        <h2 className={styles.subHeading}>{projectData.data.category.name}</h2>
+                        <h1 className={styles.heading}>{projectData.data.name}</h1>
+                        <span className={styles.authorDate}>
+                        <span className={styles.author}>{projectData.data.author.name}</span>
+                            &bull;
+                            <span className={styles.date}>{projectData.data.createdAt.toDateString()}</span>
+                        </span>
                     </div>
-                </div>
 
-                {/* Sidebar */}
-                <aside className={styles.sidebar}>
-                    {/*{project.misc.map(item => {*/}
-                    {/*    return (*/}
-                    {/*        <div className={styles.sidebarEntries}>*/}
-                    {/*            <h3 className={styles.entryTitle}>{item.title}</h3>*/}
-                    {/*            <div className={styles.sidebarItems}>*/}
-                    {/*                {item.values.map(item =>*/}
-                    {/*                    item.url ? <a href={item.url}>{item.name}</a> : <p>{item.name}</p>*/}
-                    {/*                )}*/}
-                    {/*            </div>*/}
-                    {/*        </div>*/}
-                    {/*    )*/}
-                    {/*})}*/}
-                </aside>
-            </div>
+                    {/* Main Content */}
+                    <div className={styles.project}>
+                        {/* Article */}
+                        <div className={styles.content}>
+                            <img src={featuredImageURL} alt={projectData.data.featuredImage?.alt}/>
+
+                            <div className={styles.markdown}>
+                                {/* TODO: Implement MDX Parsing */}
+                                <Markdown>{projectData.data.content}</Markdown>
+                            </div>
+                        </div>
+
+                        {/* Sidebar */}
+                        <aside className={styles.sidebar}>
+                            {/*{project.misc.map(item => {*/}
+                            {/*    return (*/}
+                            {/*        <div className={styles.sidebarEntries}>*/}
+                            {/*            <h3 className={styles.entryTitle}>{item.title}</h3>*/}
+                            {/*            <div className={styles.sidebarItems}>*/}
+                            {/*                {item.values.map(item =>*/}
+                            {/*                    item.url ? <a href={item.url}>{item.name}</a> : <p>{item.name}</p>*/}
+                            {/*                )}*/}
+                            {/*            </div>*/}
+                            {/*        </div>*/}
+                            {/*    )*/}
+                            {/*})}*/}
+                        </aside>
+                    </div>
+                </>
+            }
+            {projectData.status === "error" &&
+                <p>Error: {projectData.error}</p>
+            }
         </main>
     )
 }
