1273fdc66ae2f1bcf4cc531a4f56d6fad6c4b17f
Chris <chrismoloci@gmail.com>
2026-07-22 12:45:04 -0400

    Footer can now submit form submissions.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,38 @@
 import styles from "./Footer.module.css"
 import ContactLinks from "../ContactLinks/ContactLinks.tsx";
+import {useState} from "react";
+import {api} from "../../api/client.ts";
 
 function Footer() {
+    const [ successText, setSuccesText ] = useState<string>("");
+    const [ errorText, setErrorText ] = useState<string>("")
+
+    const onSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        try {
+            const formData = new FormData(e.target);
+
+            const email = formData.get('email');
+            const name = formData.get('name');
+            const message = formData.get('message');
+
+            if (!email || !name || !message) throw Error("Missing required fields");
+
+            const data = {
+                email, name, message
+            }
+
+            await api.post("contact", data);
+
+            setErrorText("");
+            setSuccesText("Message successfully sent!")
+        } catch (error: any) {
+            setErrorText(error.message)
+            setSuccesText("")
+        }
+    }
+
     return (
         <>
             <footer id="footer" className={styles.footer}>
@@ -16,21 +47,27 @@ function Footer() {
                         <span>or</span>
                     </span>
 
-                    <form className={styles.contactForm} action="">
+                    <p>{successText}</p>
+
+                    <form className={styles.contactForm} onSubmit={onSubmit}>
                         <label htmlFor="name">Name:
-                            <input type="text" name="name" id="name" placeholder="John Smith..." />
+                            <input type="text" name="name" id="name" placeholder="John Smith..." required />
                         </label>
 
                         <label htmlFor="email">email
-                            <input type="email" name="email" id="email" placeholder="example@example.com..." />
+                            <input type="email" name="email" id="email" placeholder="example@example.com..." required />
                         </label>
 
                         <label htmlFor="message">Message:
-                            <textarea name="message" id="message" rows={5} placeholder="message..." />
+                            <textarea name="message" id="message" rows={5} placeholder="message..." required />
                         </label>
 
                         <button type="submit">Submit</button>
                     </form>
+
+                    {errorText &&
+                        <p className={"errorText"}>{errorText}</p>
+                    }
                 </div>
 
                 <div className={styles.copyrightSection}>
