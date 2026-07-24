4cad43752d292ed93a11126afac02a6602ebd7f5
Chris <chrismoloci@gmail.com>
2026-07-24 16:23:09 -0400

    Created a proper 404 UI.

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -67,7 +67,7 @@ export const router = createBrowserRouter([
             {
                 path: "*",
                 lazy: async () => {
-                    const { default: PageNotFound } = await import("../pages/app/PageNotFoundPage/PageNotFound.tsx");
+                    const { default: PageNotFound } = await import("../pages/PageNotFoundPage/PageNotFound.tsx");
                     return { element: <PageNotFound /> }
                 }
             },
@@ -138,6 +138,13 @@ export const router = createBrowserRouter([
                         lazy: async () => {
                             const { default: Media } = await import("../pages/admin/MediaPage/Media.tsx");
                             return { element: <Media /> };
+                        }
+                    },
+                    {
+                        path: "*",
+                        lazy: async () => {
+                            const { default: PageNotFound } = await import("../pages/PageNotFoundPage/PageNotFound.tsx");
+                            return { element: <PageNotFound /> }
                         }
                     },
                 ]
